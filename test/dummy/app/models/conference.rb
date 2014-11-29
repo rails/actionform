0c980a4d50ebc3be3896038c938f8e21d9869f71
Kir Shatrov <shatrov@me.com>
2014-11-28 23:18:20 -0500

    Better file upload test, thanks to @kaspth

--- test/dummy/app/models/conference.rb
+++ test/dummy/app/models/conference.rb
@@ -1,12 +1,4 @@
 class Conference < ActiveRecord::Base
   has_one :speaker, dependent: :destroy
   validates :name, uniqueness: true
-
-  def photo=(val)
-    @photo = val.original_filename
-  end
-
-  def photo
-    @photo
-  end
 end
--- test/dummy/app/models/project.rb
+++ test/dummy/app/models/project.rb
@@ -5,4 +5,12 @@ class Project < ActiveRecord::Base
 
   has_many :project_tags
   has_many :tags, through: :project_tags
+
+  def photo=(val)
+    @photo = val.original_filename
+  end
+
+  def photo
+    @photo
+  end
 end
